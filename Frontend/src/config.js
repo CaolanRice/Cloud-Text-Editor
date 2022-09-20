d079909650c3dce5fa3a4211e8cef63f941c0d90
Caolan Rice <crice42@qub.ac.uk>
2022-09-20 09:38:18 +0100

    adding frontend

---
+++ Frontend/src/config.js
@@ -0,0 +1,3 @@
+const PROXY_URL = "http://proxyserver.40183983.qpc.hal.davecutting.uk";
+const BACKUP_URL = "http://backup-proxy.40183983.qpc.hal.davecutting.uk";
+const BACKUP_TWO_URL = "http://backup-proxy-two.40183983.qpc.hal.davecutting.uk";
